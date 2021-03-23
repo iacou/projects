9f295573e484a710272bb4d06e6942916aa9ea89
ed blake <ed@iacou.eu>
2021-03-23 13:07:59 +0200

    codefresh

--- gc-tf/modules/project/main.tf
+++ gc-tf/modules/project/main.tf
@@ -21,8 +21,14 @@ resource "google_project_service" "containerregistry" {
   disable_dependent_services = true
 }
 
+# TODO: 
 # service account - codefresh needs JSON key to grant permissions to pull from registry
 # gcloud iam service-accounts create codefresh-gcr
 # gcloud projects add-iam-policy-binding colourapi --member "serviceAccount:codefresh-gcr@colourapi.iam.gserviceaccount.com" --role "roles/storage.Admin"
 # gcloud iam service-accounts keys create keyfile.json --iam-account codefresh-gcr@colourapi.iam.gserviceaccount.com
 
+# move containers to google artifact registry instead of gcr
+# create google docker repo to store charts
+# gcloud artifacts repositories create iacou --repository-format=docker \
+# --location=us-central1 --description="docker repository"
+# enable API [artifactregistry.googleapis.com]
---
+++ kubernetes/api/codefresh/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# script to run codefresh pipeline locally for testing
+# see https://codefresh-io.github.io/cli/pipelines/run-pipeline/
+# requires codefresh CLI
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+me=`basename "$0"`
+
+codefresh run colourapi/colourapi \
+    --local \
+    -b master \
+    --variable-file $SCRIPTPATH/localvars.yaml \
+    --yaml=$SCRIPTPATH/codefresh.yaml \
+    -lv
+
+echo "$me DONE"
